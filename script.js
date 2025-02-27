8d08954fe3e53e180c73cdfc5e052dfa5e34ccf8
gtharunkumargit <152208588+gtharunkumargit@users.noreply.github.com>
2025-02-27 14:10:51 +0530

    first Change

---
+++ script.js
@@ -0,0 +1,12 @@
+function Clocks(){
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours>12?"PM":"AM";
+    hours = hours.toString().padStart(2,'0');
+    const minutes = now.getMinutes().toString().padStart(2,'0');
+    const seconds = now.getSeconds().toString().padStart(2,'0');
+    const str = `${hours}:${minutes}:${seconds} ${meridiem}`
+    document.getElementById('clock').textContent=str;
+}
+Clocks();
+setInterval(Clocks,1000);
